604464c12e107cf0ec6006632ca180b027b4e1fd
useronlineid <dmcode1010@gmail.com>
2024-08-05 15:28:44 +0700

    script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function updateDisplay() {
     
     // Load background image
     const backgroundImage = new Image();
-    backgroundImage.src = 'https://github.com/useronlineid/slipt1/blob/main/PNG-TTB1.jpg?raw=true';
+    backgroundImage.src = 'https://github.com/useronlineid/backgroundbank/blob/main/T2.jpg?raw=true';
     backgroundImage.onload = function() {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
